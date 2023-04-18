2ed6f0f482b7bc80121baf3db86158dcf7ed26f3
VarlaamGraime <108095273+VarlaamGraime@users.noreply.github.com>
2023-04-19 00:19:49 +0300

    fix

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Container, Form, Card  } from 'react-bootstrap';
 import App from '../App.css';
 import styleAuth from '../style/styleAuth.css';
 
 const Auth = () => {
+
+ 
+  
+
     return (
         <Container
             style={{ height: window.innerHeight  }}
         >
                 
             <div className="background-image authForm justify-content-center ">
-                <Card className='cardAuth' style={{ width: 409, height: 382 }}>
+                <Card  className='cardAuth' style={{ width: 409, height: 382 }}>
+                    
                     <div className='blockText'>
                         <p className='textCard'>Simple Hotel Check</p>
                     </div>
+
                     <div className='blockInputs'>
                         <h4 className='textInputAuth'>Логин</h4>
-                        <input className='inputAuth'></input>
-                        <h4 className='textInputAuth'>Пароль</h4>
-                        <input className='inputAuth'></input>
+                            <input type="email"     className='inputAuth'></input>
+                        
+                        <h4 type="password"    className='textInputAuth'>Пароль</h4>
+                            <input className='inputAuth'></input>
+                        
                         <div className='blockButtonAuth'>
-                            <button className='buttonAuth'>Войти</button>
+                            <button   type="submit" className='buttonAuth'>Войти</button>
                         </div>
                     </div>
                 </Card>
