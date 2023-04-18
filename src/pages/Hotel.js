7f1f10f0cfa5318c81065b035e651f651c63f5d4
VarlaamGraime <108095273+VarlaamGraime@users.noreply.github.com>
2023-04-18 12:55:53 +0300

    fix

--- src/pages/Hotel.js
+++ src/pages/Hotel.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Location from '../components/Location';
+import styleHotel from '../style/styleHotel.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { faSharp } from '@fortawesome/free-solid-svg-icons';
 
 const Hotel = () => {
     return (
-        <div>
-hotel
+        <div className='bgHotel'>
+            <div className='flexHeader'>
+                <h1 className='bigTextHotel'>Simple Hotel Check</h1>
+                <div className='flexExit'>
+                    <h1 className='exitText'>Выйти</h1>
+                    <FontAwesomeIcon className='exitIcon' size="lg" icon={faArrowRightFromBracket} color="#41522E" />
+                </div>
+            </div>
+            <Location />
         </div>
     );
 };
