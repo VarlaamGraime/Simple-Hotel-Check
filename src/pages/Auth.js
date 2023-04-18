a717fe29bb7618aa64004e797745c9a0b323f7cf
VarlaamGraime <108095273+VarlaamGraime@users.noreply.github.com>
2023-04-18 11:10:37 +0300

    fix

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
+import { Container, Form, Card  } from 'react-bootstrap';
+import App from '../App.css';
+import styleAuth from '../style/styleAuth.css';
 
 const Auth = () => {
     return (
         <Container
-            className=' authForm d-flex justify-content-center align-items-center'
+            style={{ height: window.innerHeight  }}
         >
-            Login
+                
+            <div className="background-image authForm justify-content-center ">
+                <Card className='cardAuth' style={{ width: 409, height: 382 }}>
+                    <div className='blockText'>
+                        <p className='textCard'>Simple Hotel Check</p>
+                    </div>
+                    <div className='blockInputs'>
+                        <h4 className='textInputAuth'>Логин</h4>
+                        <input className='inputAuth'></input>
+                        <h4 className='textInputAuth'>Пароль</h4>
+                        <input className='inputAuth'></input>
+                        <div className='blockButtonAuth'>
+                            <button className='buttonAuth'>Войти</button>
+                        </div>
+                    </div>
+                </Card>
+            </div>
+            
         </Container>
     );
 };
